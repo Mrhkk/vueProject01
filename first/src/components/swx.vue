161bf64d085f3f3bbb4b4af3977c0becc0fa58fb
lanou\lanou <3193352708@qq.com>
2019-10-10 14:53:48 +0800

    这是child1

---
+++ first/src/components/swx.vue
@@ -0,0 +1,16 @@
+<template>
+    <fieldset>
+      <leglend><h2>这是child1组件</h2></leglend>
+      <p>何凯凯是个大笨蛋</p>
+    </fieldset>
+</template>
+
+<script>
+    export default {
+        name: "swx"
+    }
+</script>
+
+<style scoped>
+
+</style>
